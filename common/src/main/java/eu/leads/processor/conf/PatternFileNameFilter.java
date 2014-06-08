d30b1ed6bfb27c1e20a7f44776ee53b45e4b04aa
Evangelos Vazaios <vagvaz@gmail.com>
2014-06-08 12:12:56 +0300

    Add LQPConfiguration class for system configuration

---
+++ common/src/main/java/eu/leads/processor/conf/PatternFileNameFilter.java
@@ -0,0 +1,20 @@
+package eu.leads.processor.conf;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * Created by vagvaz on 6/1/14.
+ */
+public class PatternFileNameFilter implements FilenameFilter {
+    private String pattern;
+
+    public PatternFileNameFilter(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.matches(pattern);
+    }
+}
