a5008316a927de16935af1c6b7df0c5f4b37338e
idemertzis <idemertzis@softnet.tuc.gr>
2014-09-07 17:47:23 +0300

    SSE v2

--- SSE_implementation/src/main/java/CStore.java
+++ SSE_implementation/src/main/java/CStore.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+package sse_implementation_trial1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,28 +9,53 @@ import java.util.HashMap;
  */
 public class CStore {
     HashMap<String, Etuple> EDB; //The encrypted Database
-    HashMap<String, ArrayList<String>> TSet; //the inverted index
+    HashMap<Integer, Record[]> TSet; //the inverted index
+    int Bvalue;
+    int Svalue;
     
     public CStore(){
         this.EDB = new HashMap<String, Etuple>();
-        this.TSet = new HashMap<String, ArrayList<String>>();
+        this.TSet = new HashMap<Integer, Record[]>();
     }
     
-    public CStore(HashMap<String, Etuple> EDB, HashMap<String, ArrayList<String>> TSet){
+    public CStore(HashMap<String, Etuple> EDB, HashMap<Integer, Record[]> TSet){
         this.EDB = EDB;
         this.TSet = TSet;
+    }
+    
+    public CStore(HashMap<String, Etuple> EDB, HashMap<Integer, Record[]> TSet, int Bvalue, int Svalue){
+        this.EDB = EDB;
+        this.TSet = TSet;
+        this.Bvalue = Bvalue;
+        this.Svalue = Svalue;
     }
     
     public HashMap<String, Etuple> getEDB(){
         return this.EDB;
     }
-    public HashMap<String, ArrayList<String>> getTSet(){
+    public HashMap<Integer, Record[]> getTSet(){
         return this.TSet;
     }
     public  void setEDB(HashMap<String, Etuple> EDB){
         this.EDB=EDB;
     }
-    public  void setTSet(HashMap<String, ArrayList<String>> TSet){
+    public  void setTSet(HashMap<Integer, Record[]> TSet){
         this.TSet=TSet;
+    }
+    
+    
+    public void setBvalue(int Bvalue){
+        this.Bvalue  =Bvalue;
+    }
+    public void setSvalue(int Svalue){
+        this.Svalue  =Svalue;
+    }
+    
+    public int getBvalue(){
+        return  this.Bvalue;
+    }
+    
+    public int getSvalue(){
+        return  this.Svalue;
     }
 }
