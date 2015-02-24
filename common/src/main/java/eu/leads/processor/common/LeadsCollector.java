b2bda1c38228e2b0293915c273ed1b5da861854d
Evangelos Vazaios <vagvaz@gmail.com>
2015-02-24 08:52:13 +0000

    Remove TMP fix that does not work for collector

--- common/src/main/java/eu/leads/processor/common/LeadsCollector.java
+++ common/src/main/java/eu/leads/processor/common/LeadsCollector.java
@@ -13,15 +13,15 @@ import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LeadsCollector<KOut, VOut> implements Collector<KOut, VOut>,
-        Serializable {
+                                                     Serializable {
 
-    private static final long serialVersionUID = -602082107893975415L;
-    private final AtomicInteger emitCount;
-    private final int maxCollectorSize;
-    private transient Cache storeCache;
-    private transient InfinispanManager imanager;
-    private boolean onMap = true;
-//  private LeadsCombiner combiner;
+  private static final long serialVersionUID = -602082107893975415L;
+  private final AtomicInteger emitCount;
+  private final int maxCollectorSize;
+  private transient Cache storeCache;
+  private transient InfinispanManager imanager;
+  private boolean onMap = true;
+  //  private LeadsCombiner combiner;
 
   public String getCacheName() {
     return cacheName;
@@ -50,93 +50,86 @@ public class LeadsCollector<KOut, VOut> implements Collector<KOut, VOut>,
   private String cacheName;
 
 
-    public LeadsCollector(int maxCollectorSize,
-                          Cache<KOut, List<VOut>> collectorCache) {
-        super();
+  public LeadsCollector(int maxCollectorSize,
+                         Cache<KOut, List<VOut>> collectorCache) {
+    super();
 
-        emitCount = new AtomicInteger();
-        this.maxCollectorSize = maxCollectorSize;
-        storeCache = collectorCache;
-        cacheName = collectorCache.getName();
-    }
+    emitCount = new AtomicInteger();
+    this.maxCollectorSize = maxCollectorSize;
+    storeCache = collectorCache;
+    cacheName = collectorCache.getName();
+  }
 
-    public LeadsCollector(int maxCollectorSize, String cacheName,InfinispanManager manager){
-      this.maxCollectorSize = maxCollectorSize;
-      emitCount = new AtomicInteger();
-      this.imanager = manager;
-      this.cacheName = cacheName;
-      storeCache = (Cache<KOut, List<VOut>>) this.imanager.getPersisentCache(cacheName);
-    }
+  public LeadsCollector(int maxCollectorSize, String cacheName,InfinispanManager manager){
+    this.maxCollectorSize = maxCollectorSize;
+    emitCount = new AtomicInteger();
+    this.imanager = manager;
+    this.cacheName = cacheName;
+    storeCache = (Cache<KOut, List<VOut>>) this.imanager.getPersisentCache(cacheName);
+  }
 
-    public Cache<KOut, List<VOut>> getCache() {
-        return storeCache;
-    }
+  public Cache<KOut, List<VOut>> getCache() {
+    return storeCache;
+  }
 
-    public void emit(KOut key, VOut value) {
-      boolean locked = false;
-      while(!locked) {
+  public void emit(KOut key, VOut value) {
 
+    if(onMap) {
+      List<VOut> list = (List<VOut>) storeCache.get(key);
 
-        locked = storeCache.getAdvancedCache().lock(key);
+      if (list == null) {
+        list = new LinkedList<>();
+        //storeCache.put(key, list);
 
       }
-      if(onMap) {
-          List<VOut> list = (List<VOut>) storeCache.get(key);
-
-          if (list == null) {
-            list = new LinkedList<>();
-            //storeCache.put(key, list);
-
-          }
-          list.add(value);
-          emitCount.incrementAndGet();
-        }
-      else{
-          storeCache.put(key,value);
-          emitCount.incrementAndGet();
-        }
-        // if (isOverflown() && mcc.hasCombiner()) {
-        // combine(mcc, this);
-        // }
-      Set<Object> keys = new HashSet<>();
-      keys.add(key);
-       storeCache.getAdvancedCache().getLockManager().unlock(keys,storeCache.getAdvancedCache());
+      list.add(value);
+      emitCount.incrementAndGet();
     }
-
-
-    public void initializeCache(EmbeddedCacheManager manager) {
-       imanager = new ClusterInfinispanManager(manager);
-        storeCache = (Cache) imanager.getPersisentCache(cacheName);
+    else{
+      storeCache.put(key,value);
+      emitCount.incrementAndGet();
     }
+    // if (isOverflown() && mcc.hasCombiner()) {
+    // combine(mcc, this);
+    // }
+    Set<Object> keys = new HashSet<>();
+    keys.add(key);
+  }
+
+
+  public void initializeCache(EmbeddedCacheManager manager) {
+    imanager = new ClusterInfinispanManager(manager);
+    storeCache = (Cache) imanager.getPersisentCache(cacheName);
+  }
 
   public void initializeCache(InfinispanManager imanager){
     this.imanager = imanager;
     storeCache = (Cache) imanager.getPersisentCache(cacheName);
   }
-    public void reset() {
-        storeCache.clear();
-        emitCount.set(0);
-    }
+  public void reset() {
+    storeCache.clear();
+    emitCount.set(0);
+  }
 
-    public void emit(Map<KOut, List<VOut>> combined) {
-        for (Entry<KOut, List<VOut>> e : combined.entrySet()) {
-            KOut k = e.getKey();
-            List<VOut> values = e.getValue();
-            for (VOut v : values) {
-                emit(k, v);
-            }
-        }
+  public void emit(Map<KOut, List<VOut>> combined) {
+    for (Entry<KOut, List<VOut>> e : combined.entrySet()) {
+      KOut k = e.getKey();
+      List<VOut> values = e.getValue();
+      for (VOut v : values) {
+        emit(k, v);
+      }
     }
+  }
 
-    public boolean isOverflown() {
-        return emitCount.get() > maxCollectorSize;
-    }
+  public boolean isOverflown() {
+    return emitCount.get() > maxCollectorSize;
+  }
 
-//  public void setCombiner(eu.leads.processor.infinispan.LeadsCombiner combiner) {
-//    this.combiner = combiner;
-//  }
-//
-//  public LeadsCombiner getCombiner() {
-//    return combiner;
-//  }
+  //  public void setCombiner(eu.leads.processor.infinispan.LeadsCombiner combiner) {
+  //    this.combiner = combiner;
+  //  }
+  //
+  //  public LeadsCombiner getCombiner() {
+  //    return combiner;
+  //  }
 }
