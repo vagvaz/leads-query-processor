9ea4a22fd38fabeb6ccb314e08c41a6f73ac5428
Evangelos Vazaios <vagvaz@gmail.com>
2015-04-14 20:42:23 +0300

    Replay test load all modules

--- leads-aux-tools/data-replay/src/test/java/ReplayTest.java
+++ leads-aux-tools/data-replay/src/test/java/ReplayTest.java
@@ -1,9 +1,13 @@
+import eu.leads.processor.conf.LQPConfiguration;
+
 /**
  * Created by vagvaz on 4/13/15.
  */
 public class ReplayTest {
    public static void main(String[] args) {
-      ReplayTool tool = new ReplayTool("/tmp/leads-crawler-snapshot/","catalog-worker-default.webpages","catalog-worker-nutchWebBackup|planner-nutchWebBackup","192.168.1.5:11222");
+      LQPConfiguration.initialize();
+      ReplayTool tool = new ReplayTool("/tmp/leads-crawler-snapshot/","catalog-worker-default.webpages","catalog-worker-nutchWebBackup|planner-nutchWebBackup|imanager-nutchWebBackup|deployer-nutchWebBackup|nqe-nutchWebBackup",
+                                              LQPConfiguration.getInstance().getConfiguration().getString("node.ip")+":11222");
       tool.replayNutch(true);
    }
 }
