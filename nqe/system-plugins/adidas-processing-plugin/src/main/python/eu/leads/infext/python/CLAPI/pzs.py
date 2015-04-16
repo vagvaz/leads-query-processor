9c1adbfec32bfe2669f3b90f9e11917319adcc5e
pskorupinski <pawel.skorupinski@gmail.com>
2015-04-16 18:46:20 +0200

    Integration - some fixes in Python calling and libs.

--- nqe/system-plugins/adidas-processing-plugin/src/main/python/eu/leads/infext/python/CLAPI/pzs.py
+++ nqe/system-plugins/adidas-processing-plugin/src/main/python/eu/leads/infext/python/CLAPI/pzs.py
@@ -46,7 +46,8 @@ def factory(str):
 def setup_custom_logger(name,fhsuffix):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    handler = logging.FileHandler('/home/nonlinear/leads-pzs-'+fhsuffix+'.log')
+    mainpythondir = os.path.dirname(os.path.realpath(__file__))+"../../../../../"
+    handler = logging.FileHandler(mainpythondir+'leads-pzs-'+fhsuffix+'.log')
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
