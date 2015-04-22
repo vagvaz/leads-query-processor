b1a8574fbaec714a1861dfb8139a734a2ca54f3f
KRambo <KRambo@users.noreply.github.com>
2015-04-22 06:54:19 +0300

    DataReplayTool fixes -added arguments

--- leads-aux-tools/data-replay/src/main/java/Replay.java
+++ leads-aux-tools/data-replay/src/main/java/Replay.java
@@ -1,17 +1,19 @@
-import eu.leads.processor.conf.LQPConfiguration;
-
 /**
  * Created by tr on 21/4/2015.
  */
 public class Replay {
 
-      public static void main(String[] args) {
-          if(args.length <1){
-              System.err.println("Please enter Ip address");
-              System.exit(0);
-          }
-        LQPConfiguration.initialize();
-          ReplayTool tool = new ReplayTool("/tmp/crawler-snapshot/","catalog-worker-default.webpages","catalog-worker-nutchWebBackup|planner-nutchWebBackup|imanager-nutchWebBackup|deployer-nutchWebBackup|nqe-nutchWebBackup",args[0]+":11222");
-          tool.replayNutch(true);
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Help, Replay args: ip path_of_data_keys delayms_per_1000_put_tocache");
+            System.exit(0);
+        }
+        int delay = 1000;
+        if (args.length == 3)
+            delay = Integer.parseInt(args[2]);
+
+        ReplayTool tool = new ReplayTool(args[1]/*"/tmp/crawler-snapshot/"*/, "catalog-worker-default.webpages", "catalog-worker-nutchWebBackup|planner-nutchWebBackup|imanager-nutchWebBackup|deployer-nutchWebBackup|nqe-nutchWebBackup", args[0] + ":11222");
+        tool.setDelay(delay);
+        tool.replayNutch(true);
     }
 }
