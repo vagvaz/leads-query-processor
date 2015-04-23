3d716bbb2643a65ce82ce4a5ed2d1810fd77d89b
pskorupinski <pawel.skorupinski@gmail.com>
2015-04-23 18:51:24 +0200

    Lines added for time measurements for created()

---
+++ nqe/system-plugins/adidas-processing-plugin/env-dependencies.sh
@@ -0,0 +1,11 @@
+mkdir -p /home/ubuntu/.adidas
+cd /home/ubuntu/.adidas
+
+git init python
+cd python
+git remote add -f origin https://github.com/vagvaz/leads-query-processor/tree/adi/nqe/system-plugins/adidas-processing-plugin/src/main/python/
+git config core.sparseCheckout true
+echo 
+cd ..
+
+
