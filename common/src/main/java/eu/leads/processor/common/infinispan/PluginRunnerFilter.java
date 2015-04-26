fda64f1f4ff7033f87b07c015ff85e722270b820
Evangelos Vazaios <vagvaz@gmail.com>
2015-04-26 14:48:10 +0100

    ADD jgroups.tmpl again and initialize on read Object of plugin runner filter

--- common/src/main/java/eu/leads/processor/common/infinispan/PluginRunnerFilter.java
+++ common/src/main/java/eu/leads/processor/common/infinispan/PluginRunnerFilter.java
@@ -87,6 +87,7 @@ public class PluginRunnerFilter implements CacheEventFilter,Serializable {
     isInitialized = false;
     UUIDname = UUID.randomUUID().toString();
     System.err.println(UUIDname+" DeSerialize " + configString);
+    initialize();
   }
 
   private void initialize() {
