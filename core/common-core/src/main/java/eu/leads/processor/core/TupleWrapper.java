50227892ef968707f818922e6bd6e3017ba5c9af
Evangelos Vazaios <vagvaz@gmail.com>
2015-08-14 17:57:27 +0100

    Make async writes in local listener

---
+++ core/common-core/src/main/java/eu/leads/processor/core/TupleWrapper.java
@@ -0,0 +1,7 @@
+package eu.leads.processor.core;
+
+/**
+ * Created by vagvaz on 8/14/15.
+ */
+public class TupleWrapper {
+}
---
+++ core/common-core/src/test/java/eu/leads/processor/core/BerkeleyDBIndex.java
@@ -0,0 +1,8 @@
+package eu.leads.processor.core;
+
+/**
+ * Created by vagvaz on 8/14/15.
+ */
+public class BerkeleyDBIndex {
+
+}
