7127d58664fbb6d8df85685736eb58b37689765a
KRambo <trs@optiplexnew>
2015-09-09 01:17:58 +0300

    Fixed no results retrieval clean up of result cache

--- interface-manager/processor-webservice/src/main/java/eu/leads/processor/web/LeadsWebServerVerticle.java
+++ interface-manager/processor-webservice/src/main/java/eu/leads/processor/web/LeadsWebServerVerticle.java
@@ -1,16 +1,19 @@
 package eu.leads.processor.web;
 
 import eu.leads.processor.conf.LQPConfiguration;
+import eu.leads.processor.core.Action;
 import eu.leads.processor.core.comp.LeadsMessageHandler;
 import eu.leads.processor.core.net.DefaultNode;
 import eu.leads.processor.core.net.Node;
+import eu.leads.processor.imanager.IManagerConstants;
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
-
 /**
  * Created by vagvaz on 8/3/14.
  */
@@ -110,17 +113,48 @@ public class LeadsWebServerVerticle extends Verticle implements LeadsMessageHand
             public void handle(HttpServerRequest httpServerRequest) {
                 httpServerRequest.response().setStatusCode(200);
                 httpServerRequest.response()
-                    .putHeader(WebStrings.CONTENT_TYPE, WebStrings.TEXT_HTML);
+                        .putHeader(WebStrings.CONTENT_TYPE, WebStrings.TEXT_HTML);
                 httpServerRequest.response()
-                    .end("<html><h1>Leads Query Processor REST Service</h1> <p>Yes I am online</p></html>");
+                        .end("<html><h1>Leads Query Processor REST Service</h1> <p>Yes I am online</p></html>");
 
             }
         });
-       matcher.post("/rest/upload/encData",uploadEncryptedData);
+       matcher.post("/rest/upload/encData", uploadEncryptedData);
        matcher.post("/rest/query/encrypted/ppq",privacyPointQueryHandler);
         vertx.createHttpServer().requestHandler(matcher)
             .listen((Integer) config.getNumber("port", 8080));
 
+        EventBus bus = vertx.eventBus();
+
+        bus.registerHandler("leads.processor.control", new Handler<Message>() {
+            @Override
+            public void handle(Message message) {
+                //System.err.println("  " + message.toString());
+
+                JsonObject body = (JsonObject) message.body();
+                if (body.containsField("type")) {
+                    if (body.getString("type").equals("action")) {
+                        Action action = new Action(body);
+                        if (!action.getLabel().equals(IManagerConstants.QUIT)) {
+
+                            System.err.println("Continue");
+                        } else {
+                            System.err.println("Exit webprocessor");
+
+                            try {
+                                Thread.sleep(1000);
+                            } catch (Exception e) {
+                                ;
+                            }
+                            System.err.println("Exit webprocessor2");
+                            stop();
+                            System.exit(0);
+                        }
+                    }
+
+                }
+            }
+        });
         container.logger().info("Webserver started");
     }
 
