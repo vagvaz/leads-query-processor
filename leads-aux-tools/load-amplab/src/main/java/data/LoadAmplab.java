ca96200f5385aed9d0c2fb839f127f179c17c920
Evangelos Vazaios <vagvaz@gmail.com>
2015-09-10 00:16:00 +0300

    discard batch from loading tools

--- leads-aux-tools/load-amplab/src/main/java/data/LoadAmplab.java
+++ leads-aux-tools/load-amplab/src/main/java/data/LoadAmplab.java
@@ -547,7 +547,7 @@ public class LoadAmplab {
             EnsembleCacheUtils.putToCacheDirect(embeddedCache,
                     ((Cache) embeddedCache).getName() + ":" + key, tuple);
         else if (ensembleCache!=null)
-            EnsembleCacheUtils.putToCache(ensembleCache, ensembleCache.getName() + ":" + key, tuple);
+            EnsembleCacheUtils.putToCacheDirect(ensembleCache, ensembleCache.getName() + ":" + key, tuple);
         try {
             Thread.sleep(delay);
         } catch (InterruptedException e) {
