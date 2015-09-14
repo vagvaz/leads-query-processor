83853501a042ff86c1ac891d13f3d7e4030aca7d
Evangelos Vazaios <vagvaz@gmail.com>
2015-09-14 17:06:22 +0300

    Push limit into Sort for less transfer data and faster Limit Operator

--- common/src/main/java/eu/leads/processor/common/infinispan/AcceptAllFilter.java
+++ common/src/main/java/eu/leads/processor/common/infinispan/AcceptAllFilter.java
@@ -9,7 +9,28 @@ import java.io.Serializable;
  * Created by vagvaz on 9/29/14.
  */
 public class AcceptAllFilter implements KeyValueFilter, Serializable {
+
+    private long threshold =-1;
+    private long counter = -1;
+    public AcceptAllFilter(){
+        threshold = -1;
+        counter = -1;
+    }
+    public AcceptAllFilter(long threshold){
+        this.threshold = threshold;
+        counter = 0;
+    }
     @Override public boolean accept(Object key, Object value, Metadata metadata) {
-        return true;
+        if (threshold < 0) {
+            return true;
+        }else{
+            if(counter < threshold){
+                counter++;
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 }
