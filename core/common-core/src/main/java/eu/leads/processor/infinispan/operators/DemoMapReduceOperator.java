e1d474de5190cda18526575723efcca509aca09b
Evangelos Vazaios <vagvaz@gmail.com>
2015-11-16 13:55:17 +0000

    Run Transform MapReduceOperator

--- core/common-core/src/main/java/eu/leads/processor/infinispan/operators/DemoMapReduceOperator.java
+++ core/common-core/src/main/java/eu/leads/processor/infinispan/operators/DemoMapReduceOperator.java
@@ -4,6 +4,9 @@ import eu.leads.processor.common.infinispan.InfinispanManager;
 import eu.leads.processor.core.Action;
 import eu.leads.processor.core.comp.LogProxy;
 import eu.leads.processor.core.net.Node;
+import eu.leads.processor.infinispan.LeadsCombiner;
+import eu.leads.processor.infinispan.LeadsReducer;
+import eu.leads.processor.infinispan.continuous.WordCountContinuousOperator;
 import eu.leads.processor.infinispan.operators.mapreduce.TransformMapper;
 import eu.leads.processor.infinispan.operators.mapreduce.TransformReducer;
 import org.vertx.java.core.json.JsonObject;
@@ -23,6 +26,38 @@ public class DemoMapReduceOperator extends MapReduceOperator {
     init_statistics(this.getClass().getCanonicalName());
   }
 
+
+
+
+  @Override public void setupMapCallable() {
+    //      init(conf);
+
+    setMapper(new TransformMapper(conf.toString()));
+    super.setupMapCallable();
+  }
+
+  @Override public void setupReduceLocalCallable() {
+    setLocalReducer(new TransformReducer(conf.toString()));
+    super.setupReduceLocalCallable();
+  }
+
+  @Override public void setupReduceCallable() {
+    setFederationReducer(new TransformReducer(conf.toString()));
+    super.setupReduceCallable();
+  }
+
+
+  public LeadsCombiner<?, ?> getCombiner() {
+    return combiner;
+  }
+
+  public void setCombiner(LeadsCombiner<?, ?> combiner) {
+    this.combiner = combiner;
+  }
+
+  public void setLocalReducer(LeadsReducer<?, ?> localReducer) {
+    this.localReducer = localReducer;
+  }
   @Override public void execute() {
     super.execute();
   }
